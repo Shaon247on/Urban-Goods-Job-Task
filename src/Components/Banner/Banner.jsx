0174d5968831fb466110ba60b2da70a94980796c
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-08-15 22:58:39 +0600

    added the banner layer based on theme

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,16 +6,16 @@ import 'animate.css';
 import useProvider from '../../Hooks/useProvider';
 
 const Banner = () => {
-    // const {theme, settheme}= useProvider()
+    const {theme}= useProvider()
     return (
         <div>
             <div className='bg-[#FDF5EC]'>                
             </div>
             <div className='relative flex justify-center mb-20 h-[575px]'>
             <video src={bannerPhoto} autoPlay loop muted  alt="" className='w-full h-[600px] absolute z-10 object-cover object-bottom'/>
-            <div className={`w-full h-[188px] md:h-[385px] lg:h-[675px] absolute z-20 bg-gradient-to-b from-#F1F1EF`}></div>
+            <div className={`w-full h-[188px] md:h-[385px] lg:h-[675px] absolute z-20 bg-gradient-to-b ${theme === 'cupcake'? "from-[#F1F1EF]": "from-black"}`}></div>
             <div className='absolute z-20 top-2 md:top-10 lg:top-20'>
-                <h1 className='text-2xl md:text-4xl lg:text-5xl font-lora text-black text-center font-semibold'>Empower Your Cloud Security <br /> Customize and Control</h1>
+                <h1 className='text-2xl md:text-4xl lg:text-5xl font-lora text-black dark:text-white text-center font-semibold'>Empower Your Cloud Security <br /> Customize and Control</h1>
             </div>
             <div className='absolute z-20 w-[300px] md:w-[461px] top-[60px] md:top-[105px] lg:top-[200px] left-10 md:left-[160px] lg:left-[440px] h-[150px] md:h-[230px] lg:h-[340px] animate__animated animate__fadeInUp text-center p-3 md:p-7 lg:p-3'>
                 <h1 className='text-2xl md:text-3xl lg:text-4xl text-black dark:text-white font-lora font-medium w-full lg:w-[250px] mx-auto'>Welcome to Future Widgets</h1>
